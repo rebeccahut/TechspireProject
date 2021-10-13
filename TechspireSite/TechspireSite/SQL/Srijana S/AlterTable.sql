b4e8cd7a9fda52bd889ad1665db42272a3d5f0f3
Srijana Shrestha <77762254+ShresthaSrijana13@users.noreply.github.com>
2021-10-13 18:26:44 -0500

    Create AlterTable.sql

---
+++ TechspireSite/TechspireSite/SQL/Srijana S/AlterTable.sql
@@ -0,0 +1,9 @@
+
+ALTER TABLE Product ADD
+	product_type_id int FOREIGN KEY REFERENCES ProductType(id),
+	product_status_id int FOREIGN KEY REFERENCES ProductStatus(id),
+	ban_reason_id int FOREIGN KEY REFERENCES BanType(id);
+
+ALTER TABLE Store ADD
+	location_id int FOREIGN KEY REFERENCES Location(id),
+	store_status_id int FOREIGN KEY REFERENCES StoreStatus(id);
