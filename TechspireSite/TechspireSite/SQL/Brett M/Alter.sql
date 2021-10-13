912f0cc61629f0e3ee3ac8c50657389bc2f4ec77
Xanjis-Maverick <78938784+BrettMeirhofer@users.noreply.github.com>
2021-10-13 16:57:31 -0500

    Updated Style Guide

--- TechspireSite/TechspireSite/SQL/Brett M/Alter.sql
+++ TechspireSite/TechspireSite/SQL/Brett M/Alter.sql
@@ -3,12 +3,10 @@ ALTER TABLE EmployeeJob ADD
     job_id int FOREIGN KEY REFERENCES Store(id),
     store_id int FOREIGN KEY REFERENCES Job(id);
 
-
 ALTER TABLE Employee ADD
     location_id int FOREIGN KEY REFERENCES Location(id),
     employee_status_id int FOREIGN KEY REFERENCES EmployeeStatus(id),
     employee_type_id int FOREIGN KEY REFERENCES EmployeeType(id);
-
 
 ALTER TABLE Location ADD
      state_id int FOREIGN KEY REFERENCES StateProvince(id);
