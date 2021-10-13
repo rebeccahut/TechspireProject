01fb13c6e6fe3d693a2965f20b3dfe354afda924
Srijana Shrestha <77762254+ShresthaSrijana13@users.noreply.github.com>
2021-10-13 18:07:42 -0500

    Update create_table.sql

--- TechspireSite/TechspireSite/SQL/Srijana S/create_table.sql
+++ TechspireSite/TechspireSite/SQL/Srijana S/create_table.sql
@@ -1,39 +1,31 @@
-CREATE TABLE product (
-					product_id INT PRIMARY KEY IDENTITY(1,1),
-					product_name VARCHAR(40) NOT NULL,
-					product_description VARCHAR (200) NOT NULL,
-					product_price DECIMAL (10,2) CHECK (product_price >= 0) NOT NULL,
-					product_type_id INT NOT NULL,
-					store_id INT NOT NULL,
-					product_status_id INT NOT NULL,
-					ban_reason_id INT );
+CREATE TABLE Product ( 
+    id int PRIMARY KEY IDENTITY(1,1), 
+    product_name nvarchar (40) NOT NULL,
+    product_desc nvarchar (200) NOT NULL, 
+    product_price numeric(19,4) CHECK (product_price >= 0) NOT NULL, 
+    ban_reason_id int
+); 
 
-CREATE TABLE product_type(
-					product_type_id INT PRIMARY KEY IDENTITY(1,1),
-					product_type_name VARCHAR(40) NOT NULL,
-					product_type_description VARCHAR(200)
-					);
+CREATE TABLE ProductType(
+    id int PRIMARY KEY IDENTITY(1,1),
+    product_type_name varchar (40) NOT NULL,
+    product_type_desc varchar (200)
+);
+
+CREATE TABLE ProductStatus (
+    id int PRIMARY KEY IDENTITY(1,1),
+    product_status_name nvarchar (50) NOT NULL,
+    product_status_desc nvarchar (200)
+);
+
+CREATE TABLE Store(
+    id INT PRIMARY KEY IDENTITY(1,1),
+    store_name nvarchar(50) NOT NULL,
+    store_phone nvarchar(14) NOT NULL,
+    store_email_address nvarchar(100) NOT NULL,
+    store_launch_date date,
+    store_close_date date
+);
 
 
-CREATE TABLE product_status (
-					product_status_id INT PRIMARY KEY IDENTITY(100,1),
-					product_status_name varchar (50) NOT NULL,
-					product_status_desc varchar (50)
-					);
-
-CREATE TABLE store(
-					store_id INT PRIMARY KEY IDENTITY(1,1),
-					store_name VARCHAR(50) NOT NULL,
-					store_status_id INT NOT NULL,
-					store_address1 VARCHAR(50) NOT NULL,
-					store_address2 VARCHAR(50),
-					store_country_id CHAR(3)NOT NULL,
-					store_city VARCHAR(50) NOT NULL,
-					store_zip CHAR(5) NOT NULL,
-					store_state_id CHAR(2) NOT NULL,
-					store_phone CHAR(10) NOT NULL,
-					store_email_address VARCHAR(100) NOT NULL,
-					store_social_media_code INT,
-					store_launch_date date,
-					store_close_date date);
 
