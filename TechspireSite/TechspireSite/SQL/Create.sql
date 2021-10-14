0e45ea5a7b8a86a9d2dcefc725e0d756373f9fe5
Xanjis-Maverick <78938784+BrettMeirhofer@users.noreply.github.com>
2021-10-13 23:35:37 -0500

    Updated Rebecca Create

--- TechspireSite/TechspireSite/SQL/Create.sql
+++ TechspireSite/TechspireSite/SQL/Create.sql
@@ -84,6 +84,29 @@ CREATE TABLE EmployeeType(
 );
 
 
+--Rebecca H
+CREATE TABLE CustomerCategory(
+    id int NOT NULL PRIMARY KEY IDENTITY(1,1),
+    category_name nvarchar(40) NOT NULL,
+    category_desc nvarchar(200)
+);
+
+CREATE TABLE CustomerCustomerCategory(
+    id int NOT NULL PRIMARY KEY IDENTITY(1,1)
+);
+
+CREATE TABLE StateProvince(
+    id int NOT NULL PRIMARY KEY IDENTITY(1,1),
+    state_province_name nvarchar(60) NOT NULL
+);
+
+CREATE TABLE Country(
+    id int NOT NULL PRIMARY KEY IDENTITY(1,1),
+    country_iso number NOT NULL,
+    country_name varchar(60) NOT NULL
+);
+
+
 --Srijana S
 CREATE TABLE Product ( 
     id int PRIMARY KEY IDENTITY(1,1), 
