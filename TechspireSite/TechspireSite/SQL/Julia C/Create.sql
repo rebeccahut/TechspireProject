5454e8b3c20165f882a2bd17a8665092185d64cc
Xanjis-Maverick <78938784+BrettMeirhofer@users.noreply.github.com>
2021-10-15 04:00:54 -0500

    Added revision notes to julia Create script

--- TechspireSite/TechspireSite/SQL/Julia C/Create.sql
+++ TechspireSite/TechspireSite/SQL/Julia C/Create.sql
@@ -7,24 +7,24 @@
     comments nvarchar(max),
     birthdate date NOT NULL,
     created_date date NOT NULL,
-    create_employee_id int(8) NOT NULL,
-    customer_status_id int(8) NOT NULL,
-    tier_id int(3) NOT NULL,
-    location_id int(8) NOT NULL,
+    create_employee_id int(8) NOT NULL, --Should be a foreign key in an alter script see other teammembers alter scripts in their folders
+    customer_status_id int(8) NOT NULL, --Alter
+    tier_id int(3) NOT NULL, --Alter
+    location_id int(8) NOT NULL, --Alter
 );
 
 CREATE TABLE OrderLine(
     id int NOT NULL PRIMARY KEY IDENTITY(1,1),
     quantity int(4)  NOT NULL DEFAULT 0,
-    ind_price numeric(19) NOT NULL DEFAULT 0,
-    total_price numeric(19) NOT NULL DEFAULT 0,
-    product_id int(8) NOT NULL,
-    order_id int(8),
+    ind_price numeric(19) NOT NULL DEFAULT 0, --Should be numeric(19,4) since the data dictionary lists a 4 decimal place constraint
+    total_price numeric(19) NOT NULL DEFAULT 0, --Should be numeric(19,4)
+    product_id int(8) NOT NULL, --Alter
+    order_id int(8), --Alter
 );
 
 CREATE TABLE CustomerReward(
     id int NOT NULL PRIMARY KEY IDENTITY(1,1),
     date_applied date NOT NULL,
-    customer_id int(8) NOT NULL,
-    reward_id int(8) NOT NULL,
+    customer_id int(8) NOT NULL, --Alter
+    reward_id int(8) NOT NULL, --Alter
 );
