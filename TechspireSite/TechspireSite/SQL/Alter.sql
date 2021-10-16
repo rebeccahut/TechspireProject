9d0ee4c5fb89cf1ce621feb9efc071c0d37aec17
Xanjis-Maverick <78938784+BrettMeirhofer@users.noreply.github.com>
2021-10-16 15:15:33 -0500

    Fixed master alter

--- TechspireSite/TechspireSite/SQL/Alter.sql
+++ TechspireSite/TechspireSite/SQL/Alter.sql
@@ -18,6 +18,13 @@ ALTER TABLE CustomerSocialMedia ADD
     customer_id int FOREIGN KEY REFERENCES Customer(id),
     social_media_type int FOREIGN KEY REFERENCES SocialMediaType(id);
 
+ALTER TABLE PointLog ADD
+    customer_id int FOREIGN KEY REFERENCES Customer(id),
+    employee_id int FOREIGN KEY REFERENCES SocialMediaType(id),
+    reason_id int FOREIGN KEY REFERENCES PointReasonType(id),
+    order_id int FOREIGN KEY REFERENCES "Order"(id),
+    customer_reward_id int FOREIGN KEY REFERENCES CustomerReward(id);
+
 --Julia C
 ALTER TABLE OrderLine ADD
     product_id int FOREIGN KEY REFERENCES Product(id),
--- TechspireSite/TechspireSite/SQL/Jade N/Alter.sql
+++ TechspireSite/TechspireSite/SQL/Jade N/Alter.sql
@@ -1,3 +1,10 @@
 ALTER TABLE CustomerSocialMedia ADD
     customer_id int FOREIGN KEY REFERENCES Customer(id),
     social_media_type int FOREIGN KEY REFERENCES SocialMediaType(id);
+
+ALTER TABLE PointLog ADD
+    customer_id int FOREIGN KEY REFERENCES Customer(id),
+    employee_id int FOREIGN KEY REFERENCES SocialMediaType(id),
+    reason_id int FOREIGN KEY REFERENCES PointReasonType(id),
+    order_id int FOREIGN KEY REFERENCES "Order"(id),
+    customer_reward_id int FOREIGN KEY REFERENCES CustomerReward(id);
