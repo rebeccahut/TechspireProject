befedbc695c9df304f8632704ab051dc4e6317d4
Jade Nguyen <70346358+jtnguyen74@users.noreply.github.com>
2021-10-16 11:22:21 -0500

    CREATE scripts
    Created PointReasonType, PointLog, SocialMediaType, CustomerSocialMedia; referenced style guide and data dictionary

---
+++ TechspireSite/TechspireSite/SQL/Jade N/CREATE.sql
@@ -0,0 +1,23 @@
+CREATE TABLE PointReasonType(
+	id int NOT NULL PRIMARY KEY IDENTITY(1,1),
+	reason_name nvarchar(40) NOT NULL,
+	reason_desc nvarchar(200) 
+);
+
+CREATE TABLE PointLog(
+	id int NOT NULL PRIMARY KEY IDENTITY(1,1),
+	quantity int NOT NULL DEFAULT 0, 
+	date date NOT NULL
+);
+
+CREATE TABLE CustomerSocialMedia(
+	id int NOT NULL PRIMARY KEY IDENTITY(1,1),
+	social_media_code nvarchar(60) NOT NULL, 
+	date_added date NOT NULL
+);
+
+CREATE TABLE SocialMediaType(
+	id int NOT NULL PRIMARY KEY IDENTITY(1,1),
+	social_media_name nvarchar(40) NOT NULL,
+	social_media_desc nvarchar(200) 
+);
