d073b2258449f6481c555bd438953e57ee098cea
hchen54 <70294661+hchen54@users.noreply.github.com>
2021-10-20 22:21:58 -0500

    Update ReportRewardsRedeemedInSixMonths.sql
    updated reports

--- TechspireSite/TechspireSite/SQL/Julia C/ReportRewardsRedeemedInSixMonths.sql
+++ TechspireSite/TechspireSite/SQL/Julia C/ReportRewardsRedeemedInSixMonths.sql
@@ -5,13 +5,14 @@
 -- Row Number,Date,Reward Name,Point Value
 SELECT ROW_NUMBER()
 OVER(ORDER BY Reward.date_added) AS ROW_NUM,
-OrderReward.date_applied AS 'Date',
+CustomerReward.date_applied AS 'Date',
 Reward.Reward_name AS 'Reward Name',
 Reward.point_cost AS 'Point Value'
 
 FROM Reward
 INNER JOIN CustomerReward ON  Reward.id = CustomerReward.reward_id 
 INNER JOIN RewardStatus ON RewardStatus.id = Reward.reward_status_id
+INNER JOIN PointLog on Reward.point_cost = PointLog.points_amount
 
 WHERE RewardStatus.is_active = 1
 AND CustomerReward.date_applied > '2012-01-01' AND
