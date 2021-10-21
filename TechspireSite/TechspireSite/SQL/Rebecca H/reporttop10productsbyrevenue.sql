2021ee7c6193e0176c1e479ed37fa03480851e08
rebeccahut <92345096+rebeccahut@users.noreply.github.com>
2021-10-21 14:40:12 -0500

    Updated reports
    from current month to historical

--- TechspireSite/TechspireSite/SQL/Rebecca H/reporttop10productsbyrevenue.sql
+++ TechspireSite/TechspireSite/SQL/Rebecca H/reporttop10productsbyrevenue.sql
@@ -10,8 +10,6 @@ From orderline as ol
 Left join "Order" as o on ol.order_id=o.id
 Left join product as p on ol.product_id=p.id
 inner join productstatus as ps on p.product_status_id=ps.id and ps.is_active=1
-Where YEAR(o.order_date) = YEAR(getdate()) 
-AND MONTH(o.order_date) = MONTH(getdate())
 Group by p.product_name
 Order by sum(ol.total_price) desc
 
