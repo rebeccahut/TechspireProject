479340245fef9bda5f3bb5bbca2b38250cbf16ba
Jade Nguyen <70346358+jtnguyen74@users.noreply.github.com>
2021-10-21 16:27:49 -0500

    Updated to show data

--- TechspireSite/TechspireSite/SQL/Saja A/Report Active Customer Points Accumulation by Month.sql
+++ TechspireSite/TechspireSite/SQL/Saja A/Report Active Customer Points Accumulation by Month.sql
@@ -4,7 +4,6 @@
 -- Displays the points accumulated by an active customer in a given month 
 -- Row Number, First name, Last name, Points Accumulated, Category name
 
-
 SELECT ROW_NUMBER() 
 OVER(ORDER BY Points."Points Accumulated" DESC) AS Row_Num, 
 Customer.first_name AS "Customer First Name", 
@@ -12,19 +11,21 @@ Customer.last_name AS "Customer Last Name",
 Points."Points Accumulated",
 CustomerCategory.category_name AS "Category"
 
+
 FROM Customer
 INNER JOIN CustomerStatus ON CustomerStatus.id = Customer.customer_status_id
-INNER JOIN (SELECT SUM(PointLog.points_amount) AS "Points Accumulated", PointLog.points_amount
+INNER JOIN "Order" ON Customer.id = "Order".customer_id
+INNER JOIN (SELECT SUM(PointLog.points_amount) AS "Points Accumulated", PointLog.order_id
 			FROM PointLog
-			WHERE PointLog.points_amount < 0
-			GROUP BY PointLog.points_amount) 
-			AS Points ON Customer.id = Points.points_amount
+			WHERE PointLog.points_amount > 0
+			GROUP BY PointLog.order_id) 
+			AS Points ON "Order".id = Points.order_id
 INNER JOIN CustomerCustomerCategory ON CustomerCustomerCategory.customer_id = Customer.id
 INNER JOIN CustomerCategory ON CustomerCustomerCategory.customer_category_id = CustomerCategory.id
 
-WHERE DATEPART(m FROM Customer.begin_date) = '11' 
+WHERE DATEPART(m FROM Customer.begin_date) = '6' 
+AND DATEPART(yyyy FROM Customer.begin_date) = '2010'
+AND CustomerStatus.id = '1'
 
-AND CustomerStatus.id = '1';
-
-
-
+GROUP BY Customer.begin_date, Customer.first_name, Customer.last_name, Points.order_id, Points."Points Accumulated", CustomerCategory.category_name
+ORDER BY MIN(Customer.begin_date);
