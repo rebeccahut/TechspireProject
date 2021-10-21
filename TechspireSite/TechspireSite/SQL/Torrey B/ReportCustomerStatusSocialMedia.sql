7f22608884e9da37b0f1a33bc25bb556ea91788e
Torrey Brett <78444099+TorreyUH@users.noreply.github.com>
2021-10-21 01:16:01 -0500

    Removed one inner join

--- TechspireSite/TechspireSite/SQL/Torrey B/ReportCustomerStatusSocialMedia.sql
+++ TechspireSite/TechspireSite/SQL/Torrey B/ReportCustomerStatusSocialMedia.sql
@@ -1,15 +1,14 @@
 --Torrey Brett
 --Customer Status and Social Media
 --The client can use this query to understand a customer Status, what social media platform they use and the date it was added.
---Displays the status of the customer and what their social media platform they are using and when it was added
+--Displays the status of the customer and what their social media platform they are using and when it was added.
 --Row Number,First Name, Last Name, Status, Social Media Name, Date
+
 
 SELECT ROW_NUMBER() OVER(ORDER BY Customer.id) AS num_row,
 Customer.first_name, Customer.last_name, CustomerStatus.status_name, SocialMediaType.Social_media_name, CustomerSocialMedia.date_added
-FROM Customer
-INNER JOIN CustomerReward
-ON Customer.id = CustomerReward.customer_id
-INNER JOIN CustomerStatus
+FROM CustomerStatus
+INNER JOIN Customer
 ON Customer.customer_Status_id = CustomerStatus.id
 INNER JOIN CustomerSocialMedia
 ON Customer.id = CustomerSocialMedia.customer_id
