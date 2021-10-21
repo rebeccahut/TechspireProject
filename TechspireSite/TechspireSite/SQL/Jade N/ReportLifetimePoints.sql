a9c0b927a20580d025ff15c0caa9dcb778508196
Jade Nguyen <70346358+jtnguyen74@users.noreply.github.com>
2021-10-21 00:10:48 -0500

    Updated LifetimePoints

--- TechspireSite/TechspireSite/SQL/Jade N/ReportLifetimePoints.sql
+++ TechspireSite/TechspireSite/SQL/Jade N/ReportLifetimePoints.sql
@@ -2,17 +2,21 @@
 --Total Points Accumulated Per Customer
 --The client can use this report to analyze which customers have accumulated the most points based on their entire order history. This will help the client easily view her most-valuable customers as well as understand the retention rate she has of her customers. 
 --Displays only the customers that have accumulated points in an order of who has accumulated the most to the least. 
---Row Number, First Name, Last Name, Order Number, Order Date, Point Reason Name, Reward Name
+--Row Number, First Name, Last Name, Order Number, Points Accumulated, Category Name
 
 SELECT ROW_NUMBER() 
-OVER(ORDER BY Customer.first_name) AS num_row, 
+OVER(ORDER BY Points."Points Accumulated" DESC) AS num_row, 
 Customer.first_name AS "Customer First Name", 
 Customer.last_name AS "Customer Last Name", 
-"Order".id AS "Order #", 
-"Order".order_date AS "Order Date",
-PointReasonType.reason_name AS "Reason"
+Points."Points Accumulated",
+CustomerCategory.category_name AS "Category"
 
-FROM PointLog
-INNER JOIN Customer ON PointLog.customer_id = Customer.id
-INNER JOIN "Order" ON PointLog.order_id = "Order".id
-INNER JOIN PointReasonType ON PointLog.reason_id = PointReasonType.id
+FROM Customer
+INNER JOIN CustomerStatus ON CustomerStatus.id = Customer.customer_status_id
+INNER JOIN (SELECT SUM(PointLog.points_amount) AS "Points Accumulated", PointLog.customer_id
+			FROM PointLog
+			WHERE PointLog.points_amount < 0
+			GROUP BY PointLog.order_id) 
+			AS Points ON Customer.id = Points.customer_id
+INNER JOIN CustomerCustomerCategory ON CustomerCustomerCategory.customer_id = Customer.id
+INNER JOIN CustomerCategory ON CustomerCustomerCategory.customer_category_id = CustomerCategory.id
