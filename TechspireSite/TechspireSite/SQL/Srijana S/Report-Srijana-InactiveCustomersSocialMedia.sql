d322c71ca65f21f9863072791a06b9ca2c6330d7
Xanjis-Maverick <brettmeirhofer@gmail.com>
2021-10-20 21:58:08 -0500

    Fixed Srijana S Reports

--- TechspireSite/TechspireSite/SQL/Srijana S/Report-Srijana-InactiveCustomersSocialMedia.sql
+++ TechspireSite/TechspireSite/SQL/Srijana S/Report-Srijana-InactiveCustomersSocialMedia.sql
@@ -2,9 +2,15 @@
 --Inactive Customers with Social Media
 --The client can use this report to view customers that have declared themselves "inactive" within the loyalty system, specifically customers that have social media. This report is beneficial for the client because the client may want to reach out to her inactive customers to resume business relations. Particularly those that use social media, it is a priority to reach out to those customers first because they might leave negative reviews. 
 --Displays only the customers that have been categorized as "inactive" with social media
---Customer First Name, Customer Last Name, Social Media Code, Social Media Type Name, City, Customer Category ID, Customer Status Name, Customer Status Description, Customer Status
+--Customer First Name, Customer Last Name, Social Media Code, Social Media Type Name, City
 
-select * from (
+select 
+A.firstname,
+A.lastname,
+A.socialmediaid,
+A.socialmedianame,
+A.city
+from (
 select Customer.customer_status_id as id,
 Customer.first_name as firstname,
 Customer.last_name as lastname,
@@ -16,6 +22,6 @@ inner join "Customer" on "CustomerSocialMedia".customer_id = "Customer".id
 inner join "location" on "Customer".location_id = "location".id
 inner join "StateProvince" on "StateProvince".id = "Location".state_id
 where "StateProvince".state_name = 'Texas'
-and "SocialMediaType".id IN (1,3)) A
+and "SocialMediaType".id IN (1,3)) AS A
 inner join "CustomerStatus" on A.id = "CustomerStatus".id
-where "CustomerStatus".id = 3
+where "CustomerStatus".is_active = 0
