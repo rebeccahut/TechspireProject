469376bca887233b5eec1e78e51ef89577724b58
rebeccahut <92345096+rebeccahut@users.noreply.github.com>
2021-10-20 22:51:00 -0500

    Updated report code
    changed order to "Order"

--- TechspireSite/TechspireSite/SQL/Rebecca H/reporttop10productcategoriesbyrevenuethismonth.sql
+++ TechspireSite/TechspireSite/SQL/Rebecca H/reporttop10productcategoriesbyrevenuethismonth.sql
@@ -7,7 +7,7 @@
 
 Select p.product_name, sum(ol.total_price) as TotalRevenue
 From orderline as ol
-Left join order as o on ol.order_id=o.id
+Left join "Order" as o on ol.order_id=o.id
 Left join product as p on ol.product_id=p.id
 inner join productstatus as ps on p.product_status_id=ps.id and ps.is_active=1
 Where YEAR(o.order_date) = YEAR(getdate()) 
