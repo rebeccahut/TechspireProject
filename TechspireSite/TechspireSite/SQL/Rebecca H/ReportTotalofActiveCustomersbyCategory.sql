f050070db365b997cc6359d3969d3722b8e4050a
rebeccahut <92345096+rebeccahut@users.noreply.github.com>
2021-10-20 21:32:13 -0500

    Updated report 1

--- TechspireSite/TechspireSite/SQL/Rebecca H/ReportTotalofActiveCustomersbyCategory.sql
+++ TechspireSite/TechspireSite/SQL/Rebecca H/ReportTotalofActiveCustomersbyCategory.sql
@@ -4,10 +4,10 @@
 --Customer Category, Total Customers
 
 
-SELECT cc.category_name, count(distinct((c.id)) as totalcustomers
-from Customer c
-inner join CustomerStatus cs on c.customer_status_id=cs.id and cs.is_active=1
-left join CustomerCustomerCategory ccc on ccc.customer_id=c.id
-left join CustomerCategory cc on cc.id=ccc.customer_category_id
+SELECT cc.category_name, count(distinct c.id) AS totalcustomers
+from Customer AS c
+inner join CustomerStatus AS cs on c.customer_status_id=cs.id and cs.is_active=1
+left join CustomerCustomerCategory AS ccc on ccc.customer_id=c.id
+left join CustomerCategory AS cc on cc.id=ccc.customer_category_id
 group by cc.category_name
-order by count(distinct((c.id)) desc
+order by count(distinct c.id) desc
