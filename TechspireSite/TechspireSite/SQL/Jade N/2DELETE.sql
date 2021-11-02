e30eeced13ba53b7cc5ddef227635eb6651e4133
Jade Nguyen <70346358+jtnguyen74@users.noreply.github.com>
2021-11-01 22:07:42 -0500

    Updated with comments

--- TechspireSite/TechspireSite/SQL/Jade N/2DELETE.sql
+++ TechspireSite/TechspireSite/SQL/Jade N/2DELETE.sql
@@ -1,5 +1,7 @@
+-- deletes extraneous point types from insert
 DELETE FROM PointReasonType 
-WHERE id > 5 -- deletes extraneous point types from insert
+WHERE id > 5 
 
+-- deletes any customer social media data before 2017 because this is, realistically, when she started her loyalty system
 DELETE FROM CustomerSocialMedia
-WHERE date_added < '2017-01-01' -- deletes any customer social media data before 2017 because this is, realistically, when she started her loyalty system
+WHERE date_added < '2017-01-01' 
