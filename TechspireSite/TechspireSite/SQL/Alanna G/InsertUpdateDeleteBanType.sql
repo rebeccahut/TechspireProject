af25ad380bf92793ef01f3c9ffccf1a069068426
aigilcre <92191915+aigilcre@users.noreply.github.com>
2021-11-02 20:11:28 -0500

    InsertUpdateDelete

---
+++ TechspireSite/TechspireSite/SQL/Alanna G/InsertUpdateDeleteBanType.sql
@@ -0,0 +1,16 @@
+--Alanna Gilcrease
+--Purpose to:
+--Insert an attribute for Seasonal products to BanType table
+--Update column name from Expensive to No Points in BanType table
+--Delete the Seasonal attribute from BanType table 
+
+--INSERT INTO BanType (ban_name, ban_desc)
+--VALUES ('Seasonal','Limited-time offer')
+
+--UPDATE BanType
+--SET ban_name = 'No Points'
+--WHERE id = 1
+--SELECT * FROM BanType WHERE ban_name = 'Expensive'
+
+--DELETE FROM BanType
+--WHERE id=4
---
+++ TechspireSite/TechspireSite/SQL/Alanna G/InsertUpdateDeleteEmployeeStatus.sql
@@ -0,0 +1,17 @@
+--Alanna Gilcrease
+--Purpose to:
+--Insert an attribute to EmployeeStatus table
+--Update a column name in EmployeeStatus table
+--Delete an attribute on EmployeeStatus table
+
+--INSERT INTO EmployeeStatus (status_name, status_desc, is_active)
+--VALUES ('Seasonal', 'Temporary employee during holiday rush', 0)
+
+--UPDATE EmployeeStatus
+--SET status_name = 'Temp'
+--WHERE id = 10
+--SELECT * FROM EmployeeStatus WHERE status_name = 'Seasonal'
+
+--DELETE FROM EmployeeStatus
+--WHERE id = 10
+
