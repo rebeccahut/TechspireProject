346180db8b8f08b4e1dbe53dfda1cfae134f87a9
aigilcre <92191915+aigilcre@users.noreply.github.com>
2021-11-06 20:26:52 -0500

    Update ReportCurrentActiveEmployee.sql

--- TechspireSite/TechspireSite/SQL/Alanna G/ReportCurrentActiveEmployee.sql
+++ TechspireSite/TechspireSite/SQL/Alanna G/ReportCurrentActiveEmployee.sql
@@ -2,22 +2,38 @@
 --Current Active Employees
 --The client could use this report to view a list of current active employees.
 --For example, the client could use this report to call employees in the event of an emergency or to call someone in to work if shorthanded.
---Displays all active employees per store
---Row Number, First Name, Last Name, Status, Store
+--This report displays all active employees, phone number, their job title, and store location.  
+--Row Number, Employee ID, First Name, Last Name, Phone Number, Job Title, Status, and Store
 
-SELECT ROW_NUMBER () OVER(ORDER BY Employee.last_name) AS 'Row Num',
-Employee.id AS 'ID',
+SELECT ROW_NUMBER () OVER(ORDER BY Job.job_name, Employee.last_name) AS 'Row Number',
+Employee.id AS 'Employee ID',
 Employee.first_name AS 'First Name',
 Employee.last_name AS 'Last Name',
-EmployeeJob.job_id AS 'Job Title',
-EmployeeStatus.is_active AS 'Status'
+Employee.phone_number AS 'Phone Number',
+Job.job_name AS 'Job Title',
+EmployeeStatus.is_active AS 'Status',
+EmployeeJob.store_id AS 'Store'
+
 
 FROM Employee
-INNER JOIN EmployeeJob ON EmployeeJob.employee_id = Employee.id
-INNER JOIN Job ON EmployeeJob.job_id = Job.id
-INNER JOIN EmployeeType ON Employee.employee_type_id = EmployeeType.id
-INNER JOIN EmployeeStatus ON Employee.employee_status_id = EmployeeStatus.id
-INNER JOIN EmployeeEmployeeCategory ON EmployeeEmployeeCategory.employee_id = Employee.id
-INNER JOIN EmployeeCategory ON EmployeeEmployeeCategory.employee_category_id = EmployeeCategory.id
 
-WHERE EmployeeStatus.id = 1
+INNER JOIN EmployeeJob 
+ON EmployeeJob.employee_id = Employee.id
+
+INNER JOIN Job 
+ON EmployeeJob.job_id = Job.id
+
+INNER JOIN EmployeeType
+ON Employee.employee_type_id = EmployeeType.id
+
+INNER JOIN EmployeeStatus
+ON Employee.employee_status_id = EmployeeStatus.id
+
+INNER JOIN EmployeeEmployeeCategory 
+ON EmployeeEmployeeCategory.employee_id = Employee.id
+
+INNER JOIN EmployeeCategory 
+ON EmployeeEmployeeCategory.employee_category_id = EmployeeCategory.id
+
+WHERE EmployeeStatus.id = 1 AND EmployeeJob.store_id = 1
+
