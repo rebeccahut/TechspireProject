f4e81b61a64b19cfc3332494c6ff9a6bb3996e7f
Srijana Shrestha <77762254+ShresthaSrijana13@users.noreply.github.com>
2021-11-10 15:24:28 -0600

    Report 2 and renamed the report 1

--- TechspireSite/TechspireSite/SQL/Srijana S/Report-Srijana-ActiveCustomersBirthdayMonth.sql
+++ TechspireSite/TechspireSite/SQL/Srijana S/Report-Srijana-ActiveCustomersBirthdayMonth.sql
@@ -1,23 +1,23 @@
 --Srijana Shrestha
---Active Customers with a Specific Birth Month
---The client can use this report to view customers that all have the same birth month depending on a the client's selected month. This report is beneficial for the client because the client is able to view customers who were born in the current month or next month, and from there, be able to anticipate a rise of reward usages. Furthermore, the client will be able to view all the customer emails in order to send them marketing emails for their birthday. 
---Displays only the customers that were born in the month of March and active. It is ordered by the customer's birthdate. Information about the customer's address is included(if avaliable) along with how many orders the customer has historically.
---Full Name,Email Address,Birth Date,Address,Zip_code,City Name,Total Transactions
+--Active Customer with a specific Birth Month
+--The client can use this report to view customers that all have the same birth month from all the stores, depending on the client's selected month. This report is beneficial for the client because the client can view customers born in the current month or next month and anticipate a rise in reward usages. Furthermore, the client will be able to view all the customer emails to send them marketing emails for their birthday.
+--Displays only the customers that were born in the specific month
+--Customer full name, Email Address, Date of Birth, Store Name, Customer Status
+--,,,,,
+DECLARE
+@customer_birthmonth INT
+SELECT @customer_birthmonth = 8
 
-Declare @customer_birthmonth INT = 3
-select "Customer".first_name + ' ' + "Customer".last_name AS "Customer_full_name",
-"Customer".email_address,
-"Customer".birthdate
-,"Location"."address"
-,"Location".zip_code
-,"Location".city
-,Transactions.total
-from "Customer" 
-INNER JOIN CustomerStatus ON Customer.customer_status_id = CustomerStatus.id
-LEFT JOIN "Location" ON Customer.location_id = "Location".id
-INNER JOIN (SELECT Count(*) AS total, "Order".customer_id
-FROM "Order"
-GROUP BY "Order".customer_id
-)AS Transactions ON Transactions.customer_id = Customer.id
-WHERE month("Customer".birthdate) = @customer_birthmonth AND
-CustomerStatus.is_active = 1
+SELECT DISTINCT "Customer".id AS "Customer_Id",
+"Customer".first_name + ' ' + "Customer".last_name AS "Customer_Full_Name",
+"Customer".email_address AS "Email Address",
+"Customer".birthdate AS "Date_of_Birth",
+"Store".store_name AS "Store_Name" ,
+"CustomerStatus".status_name AS "Customer_status"
+FROM "Store" INNER JOIN "Order"ON"Store".id = "Order".store_id
+INNER JOIN "Customer"ON"Order".customer_id = "Customer".id
+INNER JOIN "CustomerStatus"ON "Customer".customer_status_id = "CustomerStatus".id
+where "CustomerStatus".id =1 and
+month("Customer".birthdate) = @customer_birthmonth
+order by "Customer".birthdate
+
--- TechspireSite/TechspireSite/SQL/Srijana S/Report-Srijana-InactiveCustomersSocialMedia.sql
+++ TechspireSite/TechspireSite/SQL/Srijana S/Report-Srijana-InactiveCustomersSocialMedia.sql
@@ -1,27 +1,25 @@
 --Srijana Shrestha
---Inactive Customers with Social Media
---The client can use this report to view customers that have declared themselves "inactive" within the loyalty system, specifically customers that have social media. This report is beneficial for the client because the client may want to reach out to her inactive customers to resume business relations. Particularly those that use social media, it is a priority to reach out to those customers first because they might leave negative reviews. 
---Displays only the customers that have been categorized as "inactive" with social media
---Customer First Name, Customer Last Name, Social Media Code, Social Media Type Name, City
+--Inactive-Cancel Customer with Social media details
+--The client can use this report to view customers that have declared themselves "inactive" within the loyalty system, specifically customers that have social media. This report is beneficial for the client because the client may want to reach out to her inactive customers to resume business relations. Particularly for those that use social media, it is a priority to reach out to those customers first because they might leave negative reviews.
+--Displays only the customers that have been categorized as "inactive-cancel" with social media
+--Customer Full Name, Customer Social Media Id, Social Media Name, City, Customer Status, Customer Status Desc
+--,,,,,
 
-select 
-A.firstname,
-A.lastname,
-A.socialmediaid,
-A.socialmedianame,
-A.city
-from (
-select Customer.customer_status_id as id,
-Customer.first_name as firstname,
-Customer.last_name as lastname,
-CustomerSocialMedia.social_media_code as socialmediaid,
-SocialMediaType.social_media_name as socialmedianame,
-city as city
-from "SocialMediaType" inner join "CustomerSocialMedia" on "SocialMediaType".id = "CustomerSocialMedia".social_media_type_id
-inner join "Customer" on "CustomerSocialMedia".customer_id = "Customer".id
-inner join "location" on "Customer".location_id = "location".id
-inner join "StateProvince" on "StateProvince".id = "Location".state_id
-where "StateProvince".state_name = 'Texas'
-and "SocialMediaType".id IN (1,3)) AS A
-inner join "CustomerStatus" on A.id = "CustomerStatus".id
-where "CustomerStatus".is_active = 0
+SELECT Customer_Full_Name, Customer_Social_Media_Id, Social_Media_Name, City, 
+status_name AS "Customer_Status", status_desc AS "Customer_Status_Desc" FROM (
+
+SELECT Customer.customer_status_id AS id,
+"Customer".first_name + ' ' + "Customer".last_name AS "Customer_Full_Name",
+CustomerSocialMedia.social_media_code AS "Customer_Social_Media_Id",
+SocialMediaType.social_media_name AS "Social_Media_Name",
+city AS "City"
+FROM "SocialMediaType" 
+INNER JOIN"CustomerSocialMedia"ON"SocialMediaType".id = "CustomerSocialMedia".social_media_type_id
+INNER JOIN"Customer"ON"CustomerSocialMedia".customer_id = "Customer".id
+INNER JOIN"location"ON"Customer".location_id = "location".id
+INNER JOIN"StateProvince"ON"StateProvince".id = "Location".state_id
+WHERE "StateProvince".state_name = 'Texas'
+and "SocialMediaType".id IN (1,3)) A
+
+INNER JOIN"CustomerStatus" ON A.id = "CustomerStatus".id
+where "CustomerStatus".id = 3
