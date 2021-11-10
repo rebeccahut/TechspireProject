0471e023d0a7f786b6066358feb72cba788f671b
Xanjis-Maverick <78938784+BrettMeirhofer@users.noreply.github.com>
2021-11-10 14:02:51 -0600

    Revert "Update ReportTotalofActiveCustomersbyCategory.sql"
    This reverts commit 2af474da0e2e62e81c180bee9c6187744540fa3d.

--- TechspireSite/TechspireSite/SQL/Rebecca H/ReportTotalofActiveCustomersbyCategory.sql
+++ TechspireSite/TechspireSite/SQL/Rebecca H/ReportTotalofActiveCustomersbyCategory.sql
@@ -1,15 +1,15 @@
 --Rebecca Hutchinson
 --Total active customers by customer category
---This report will allow for the manager to access their current loyalty members and what categories they belong too. The client can create her own categories for her customers. Based on this report, the client can easily see how many of her customers fit into which of her created categories. This report would assist in developing customer insights for potentially creating specialized discounts. For instance, if there are many “unhappy customers” then this would create aware for her and her business operations. 
+--This report will allow for the manager to access their current loyalty members and what categories they belong too.
 --Display active customers by the customer category they have been assigned ordered by amount in each category
 --Customer Category, Total Customers
 --,,
 
 
-SELECT cc.category_name, COUNT(DISTINCT c.id) AS Totalcustomers
-FROM Customer AS c
-INNER JOIN CustomerStatus AS cs ON c.customer_status_id=cs.id AND cs.is_active=1
-LEFT JOIN CustomerCustomerCategory AS ccc ON ccc.customer_id=c.id
-LEFT JOIN CustomerCategory AS cc ON cc.id=ccc.customer_category_id
-GROUP BY cc.category_name
-ORDER BY COUNT(DISTINCT c.id) DESC
+SELECT cc.category_name, count(distinct c.id) AS totalcustomers
+from Customer AS c
+inner join CustomerStatus AS cs on c.customer_status_id=cs.id and cs.is_active=1
+left join CustomerCustomerCategory AS ccc on ccc.customer_id=c.id
+left join CustomerCategory AS cc on cc.id=ccc.customer_category_id
+group by cc.category_name
+order by count(distinct c.id) desc
