7c0ccf8da13fbd67b0136c9244aa3afbdae91040
Jade Nguyen <70346358+jtnguyen74@users.noreply.github.com>
2021-11-10 15:07:23 -0600

    Update reporttop10productsbyrevenue.sql

--- TechspireSite/TechspireSite/SQL/Rebecca H/reporttop10productsbyrevenue.sql
+++ TechspireSite/TechspireSite/SQL/Rebecca H/reporttop10productsbyrevenue.sql
@@ -1,16 +1,15 @@
 --Rebecca Hutchinson
 --Top 10 products by revenue this month
---This report is for the manager to show the top 10 products by revenue.
+--This report is for the client to show the top 10 products by revenue. The client can understand popular individual items amongst customers and use that knowledge to add additional rewards. The client can also use this report to find potential differences from Top 10 Product Categories by Revenue to see if there are any outliers from category to individual products.
 --Display the product name ordered by the total revenue to understand the spread of products on orders. This can help determine which products are popular to purchase.
 --Product name, Total Revenue 
 --,right
 
 
-Select Top 10 (concat('$', cast(sum(ol.total_price) AS decimal(18,2)))) as TotalRevenue, p.product_name
-From orderline as ol
-Left join "Order" as o on ol.order_id=o.id
-Left join product as p on ol.product_id=p.id
-inner join productstatus as ps on p.product_status_id=ps.id and ps.is_active=1
-Group by p.product_name
-Order by sum(ol.total_price) desc
-
+SELECT TOP 10 p.product_name, (CONCAT('$', CAST(SUM(ol.total_price) AS DECIMAL(18,2)))) AS TotalRevenue
+FROM orderline AS ol
+LEFT JOIN "Order" AS o ON ol.order_id=o.id
+LEFT JOIN product AS p ON ol.product_id=p.id
+INNER JOIN productstatus AS ps ON p.product_status_id=ps.id and ps.is_active=1
+GROUP BY p.product_name
+ORDER BY SUM(ol.total_price) DESC
