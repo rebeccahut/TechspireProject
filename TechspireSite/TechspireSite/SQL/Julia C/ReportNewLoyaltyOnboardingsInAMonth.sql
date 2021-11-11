13825eb7750697519dc0bf5a658fb4042e441849
hchen54 <70294661+hchen54@users.noreply.github.com>
2021-11-11 12:07:07 -0600

    updated reports

--- TechspireSite/TechspireSite/SQL/Julia C/ReportNewLoyaltyOnboardingsInAMonth.sql
+++ TechspireSite/TechspireSite/SQL/Julia C/ReportNewLoyaltyOnboardingsInAMonth.sql
@@ -1,27 +1,29 @@
 -- Julia Chen
--- Loyalty Members Joined in a given month, by day
--- client can use report to view the specific members that joined in a month
--- displays new members joined in a month, by day
--- ROW_NUM,First Name,Last Name,Points Earned,Employee First Name,Employee Last Name
+--  Loyalty Members Based on Specific Customer Tier and Active Status
+-- The client can use this report to view members that belong to a specific tier and their activity status. In the screenshot the report displays customers that belong to the Bronze tier and are currently active customers, as well as their basic personal information. These reports can let her quickly view a list of customers that belong to a particular tier and allows her to target her promotions to customers that fit a specific criteria. For example, she may decide to target her promotions towards active Bronze Tier customers by offering them limited-time offers such as discounts or coupons in an effort to incentivize them into increasing their spending.
+-- This report displays loyalty members of a specified tier and active status, and displays their member information such as their full name, birthday, email phone number, current status and the loyalty tier they belong to.
+-- ROW_NUM,Full Name,Birthday,Email,Phone #,Current Status,Current Tier
+-- ,,,,phone,,
+
+DECLARE @tier_name VARCHAR(200) = 'Silver'
+DECLARE @current_status_id VARCHAR(200) = '1'
 
 SELECT ROW_NUMBER() 
-OVER(ORDER BY Customer.begin_date) AS ROW_NUM,
-Customer.begin_date AS 'Date',
-Customer.first_name AS 'First Name',
-Customer.last_name AS 'Last Name',
-PointLog.points_amount AS 'Points Earned',
-Employee.first_name AS 'Employee First Name',
-Employee.last_name AS 'Employee Last Name'
+OVER(ORDER BY Customer.first_name) AS ROW_NUM,
+Customer.first_name + ' ' + Customer.last_name AS 'Full Name',
+Customer.birthdate AS ' Birthday',
+Customer.email_address AS 'Email',
+Customer.phone_number AS 'Phone #',
+CustomerStatus.status_name AS 'Current Status',
+Tier.tier_name AS 'Current Tier'
 
 
 FROM Customer
-JOIN PointLog ON Customer.id = PointLog.customer_id
-JOIN Employee ON Customer.create_employee_id = Employee.id
 JOIN CustomerStatus ON Customer.customer_status_id = CustomerStatus.id
+JOIN Tier ON Customer.tier_id = Tier.id
+JOIN "Order" ON Customer.id = "Order".customer_id
+WHERE CustomerStatus.id = @current_status_id
+AND Tier.tier_name = @tier_name
 
-WHERE DATEPART(m FROM Customer.begin_date) = '10' 
-AND DATEPART(yyyy FROM Customer.begin_date) = '2017'
-AND CustomerStatus.id = '1'
-
-GROUP BY Customer.begin_date, Customer.first_name, Customer.last_name, points_amount,Employee.first_name,Employee.last_name
-ORDER BY MIN(Customer.begin_date);
+GROUP BY Customer.begin_date, Customer.first_name, Customer.last_name, Customer.birthdate, Customer.email_address, Customer.phone_number, CustomerStatus.status_name, Customer.point_total, Tier.tier_name
+ORDER BY Customer.last_name, Customer.first_name;	
--- TechspireSite/TechspireSite/SQL/Julia C/ReportRewardsRedeemedInSixMonths.sql
+++ TechspireSite/TechspireSite/SQL/Julia C/ReportRewardsRedeemedInSixMonths.sql
@@ -4,20 +4,26 @@
 -- Displays all rewards redeemed by customers over a given six month timeframe
 -- Row Number,Date,Reward Name,Point Value
 SELECT ROW_NUMBER()
-OVER(ORDER BY OrderReward.date_applied) AS ROW_NUM,
-OrderReward.date_applied AS 'Date',
+OVER(ORDER BY "Order".order_date) AS ROW_NUM,
 Reward.Reward_name AS 'Reward Name',
 Reward.point_cost AS 'Point Value',
+"Order".order_date AS 'Date',
 COUNT(Reward.reward_name) AS 'Times Redeemed' 
 
 FROM Reward
 INNER JOIN OrderReward ON  Reward.id = OrderReward.reward_id
-INNER JOIN Customer ON OrderReward.customer_id = Customer.id
-INNER JOIN "Order" ON "Order".customer_id = Customer.id
-INNER JOIN RewardStatus ON RewardStatus.id = Reward.reward_status_id
-INNER JOIN PointLog on Reward.point_cost = PointLog.points_amount 
+--INNER JOIN Customer ON OrderReward.customer_id = Customer.id
+--INNER JOIN OrderReward ON OrderReward.id = Reward.order_id
+--INNER JOIN "Order" ON "Order".customer_id = Customer.id
+--INNER JOIN RewardStatus ON RewardStatus.id = Reward.reward_status_id
+--INNER JOIN PointLog on Reward.point_cost = PointLog.points_amount 
 
-WHERE RewardStatus.is_active = 1
-AND "Order".order_date > '2012-01-01' AND "Order".order_date < '2012-06-30'
 
-GROUP BY OrderReward.date_applied, Reward.reward_name, Reward.point_cost
+
+--WHERE RewardStatus.is_active = 1
+--AND "Order".order_date > '2012-01-01' AND "Order".order_date < '2012-06-30'
+--AND DATEPART(m FROM Customer.begin_date) = '10' 
+--AND DATEPART(yyyy FROM Customer.begin_date) = '2017'
+
+
+GROUP BY Reward.reward_name, Reward.point_cost
