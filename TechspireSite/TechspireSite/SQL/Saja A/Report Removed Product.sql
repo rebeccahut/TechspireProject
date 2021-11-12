dd023650476593bc58edaa9f9417f1b7d453ae9f
Saja Alyasin <70542385+sajaalyasin@users.noreply.github.com>
2021-11-12 00:35:28 -0600

    Update Report Removed Product.sql

--- TechspireSite/TechspireSite/SQL/Saja A/Report Removed Product.sql
+++ TechspireSite/TechspireSite/SQL/Saja A/Report Removed Product.sql
@@ -1,7 +1,7 @@
 -- Saja Alyasin
 -- Removed Products
--- The client can use this report to determine the what products were removed in order to get a better idea of what product types to make less of.
--- Displays the removed products by ascending product name
+-- The client can utilize this report to determine what products were removed in order to get a better idea of what product types to make less of. For an example since all products are divided into categories such as savory, sweet, etc., this report displays the product and the product type that were removed due to inactivity in regard to sales, if most of the product types that were removed are savory products then the individual viewing this report will be able to which product type is least popular and take further action. This report will serve a greater purpose and be more beneficial in the future when products become inactive, discontinued or removed.
+-- This report displays the removed products by ascending product name.
 -- Row Number, Product Name, Product Type, Product Status, Inactivity Reason, Store 
 --,,,,,
 
@@ -19,4 +19,4 @@ INNER JOIN BanType ON Product.ban_reason_id = BanType.id
 INNER JOIN StoreProduct ON Product.id = StoreProduct.product_id
 INNER JOIN Store ON StoreProduct.store_id = Store.id
 
---no inactive products at the moment, so there is no output
+
