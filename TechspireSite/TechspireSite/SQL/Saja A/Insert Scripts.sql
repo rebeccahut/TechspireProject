edda64ed1b571cdec9d63cd8548cab2137aa07ef
Saja Alyasin <70542385+sajaalyasin@users.noreply.github.com>
2021-11-14 15:21:14 -0600

    Update Insert Scripts.sql

--- TechspireSite/TechspireSite/SQL/Saja A/Insert Scripts.sql
+++ TechspireSite/TechspireSite/SQL/Saja A/Insert Scripts.sql
@@ -1,5 +1,5 @@
-INSERT INTO StoreSocialMedia(social_media_code, social_media_type_id, store_id, date_added)
-VALUES ('hotbreadshouston', 2, 1, '2021-11-02')
+INSERT INTO StoreSocialMedia(social_media_code, date_added, store_id, social_media_type_id) 
+VALUES ('hotbreadshouston', '2021-11-01', 1, 2); 
 
-INSERT INTO EmployeeSocialMedia(social_media_code, social_media_type_id, employee_id, date_added)
-VALUES ('EmilyDoven', 1, 102, '2021-11-02')
+INSERT INTO EmployeeSocialMedia(social_media_code, date_added, employee_id, social_media_type_id) 
+VALUES ('EmilyDoven', '2021-11-01', 21, 2); 
