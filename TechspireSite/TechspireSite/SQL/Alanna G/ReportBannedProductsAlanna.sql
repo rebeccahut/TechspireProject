ff3a358532a35c68ea0792f3a60c7c9cd147a174
Srijana Shrestha <77762254+ShresthaSrijana13@users.noreply.github.com>
2021-11-14 19:55:17 -0600

    fixed

--- TechspireSite/TechspireSite/SQL/Alanna G/ReportBannedProductsAlanna.sql
+++ TechspireSite/TechspireSite/SQL/Alanna G/ReportBannedProductsAlanna.sql
@@ -3,10 +3,10 @@
 --This report allows the client to view all banned products. These products are either too expensive or for a custom order.
 --The client may choose to use this report to add or remove products from the list of banned products. The client can verify if the item is active or inactive.
 --Row Num, Product ID, Product Type, Product Name, Ban Reason, Product Status, Product Price
---,,,,,,right
+--,,,,,right
 
 SELECT ROW_NUMBER () OVER(ORDER BY Product.product_name, Product.product_price) AS 'Row Num',
-Product.id AS 'Product ID',
+
 ProductType.product_type_name AS 'Product Type',
 Product.product_name AS 'Product Name',
 BanType.ban_name AS 'Ban Reason',
