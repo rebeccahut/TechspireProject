267a204f1f6a4fd9a42daad942fdfe884776f582
Srijana Shrestha <77762254+ShresthaSrijana13@users.noreply.github.com>
2021-11-14 19:11:20 -0600

    ss

--- TechspireSite/TechspireSite/SQL/Srijana S/Report-Srijana-InactiveCustomersSocialMedia.sql
+++ TechspireSite/TechspireSite/SQL/Srijana S/Report-Srijana-InactiveCustomersSocialMedia.sql
@@ -2,13 +2,13 @@
 --Inactive-Cancel Customer with Social media details
 --The client can use this report to view customers that have declared themselves "inactive" within the loyalty system, specifically customers that have social media. This report is beneficial for the client because the client may want to reach out to her inactive customers to resume business relations. Particularly those that use social media, it is a priority to reach out to those customers first because they might leave negative reviews.
 --Displays only the customers that have been categorized as "inactive-cancel" with social media handle
---Customer Full Name,Customer Social Media Id,Social Media Name, City,Customer Status,Customer Status Desc
---,,,,,
-
-SELECT Full_Name, Customer_Handle, Social_Media, City, 
+--Full Name,Email,Social Media,Customer Handle, City,Customer Status,Status Desc
+--,,,,,,
+SELECT Full_Name, Email, Social_Media, Customer_Handle, City, 
 status_name AS "Customer_Status", status_desc AS "Status_Desc" FROM (
 
 SELECT Customer.customer_status_id AS id,
+Customer.email_address AS Email,
 Customer.first_name + ' ' + "Customer".last_name AS "Full_Name",
 CustomerSocialMedia.social_media_code AS "Customer_Handle",
 SocialMediaType.social_media_name AS "Social_Media",
